f061c8472bf1318eaadc47b4affe237ffd5bc4b5
Oege Hiddema <oegehiddema@gmail.com>
2019-05-06 21:41:34 +0200

    insert an extra button (DEL), which deletes the last character

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ function getResult() {
   display.innerHTML = eval(display.innerHTML);
 }
 
+function delLastChar() {
+  var newStr = ''
+  var str = display.innerHTML;
+  if (str.length > 0) {
+    newStr = str.slice(0,str.length-1);
+  }
+  display.innerHTML = newStr;
+}
+
 // jQuery
 $(function () {
   $('.addTo').click(function () {
