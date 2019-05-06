d7af0d4e27d1c8537ac18c91bd53cb970410d6cd
Oege Hiddema <oegehiddema@gmail.com>
2019-05-06 21:17:32 +0200

    add some styling

--- script.js
+++ script.js
@@ -12,8 +12,17 @@ function getResult() {
   display.innerHTML = eval(display.innerHTML);
 }
 
+// jQuery
 $(function () {
   $('.addTo').click(function () {
     display.innerHTML += this.innerHTML;
   });
+
+  $('.hover').mouseenter(function () {
+    $(this).css('backgroundColor', 'grey');
+  })
+
+  $('.hover').mouseleave(function () {
+    $(this).css('backgroundColor', 'lightgrey');
+  })
 });
